e6f34e5407404ba030ac831f50d1120deb555e27
martinjras <martinjras@gmail.com>
2015-04-17 07:33:28 +0200

    Adding shell method for doing something usefull

---
+++ ConfluencePowerShellModule/Something.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+
+namespace ConfluencePowerShellModule
+{
+    [Cmdlet(VerbsCommon.Get, "Spaces")]
+    public class GetSpaces : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            HelpMessage = "Provide the AuthToken issued by calling Get-AuthToken")]
+        public string AuthToken { get; set; }
+
+        private ConfluenceServiceWrapper _wrapper;
+
+        protected override void BeginProcessing()
+        {
+            _wrapper = new ConfluenceServiceWrapper();
+        }
+
+        protected override void ProcessRecord()
+        {
+            _wrapper.GetSpaces(AuthToken);
+        }
+    }
+}
